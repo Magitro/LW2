8692a96d983c5bd47f3e06993438d756279eb1e6
Magitro <96024554+Magitro@users.noreply.github.com>
2021-12-28 21:17:49 +0300

    Add files via upload

---
+++ User.php
@@ -0,0 +1,27 @@
+<?php
+    $Firstname = ($_POST['Fname']);
+    $Lasttname = ($_POST['Lname']);
+    if($_POST['patronymic'] != "")
+    {
+        $Patronymic = $_POST['patronymic'];
+    }
+    $to = "gorhayrapetyan2003@gmail.com";
+    $from = trim($_POST['email']);
+
+    $massage = htmlspecialchars($_POST['massage']);
+    $massage = urldecode($massage);
+    $massage = trim($massage);
+
+    $headers = "From: $from" . "\r\n" .
+    "Reply-To: $from" . "\r\n" .
+    "X-Mailer: PHP/" . phpversion();
+    
+    $subject = 'Соревнование';
+
+    if(mail($to, $subject, $massage, $headers))
+    {
+        echo 'Письмо отправлено';
+    }else{
+        echo 'Письмо не отправлено';
+    }
+?>
